1a95485ecd39fbb8d2015d378cce759e5d111e09
Bhuvaneswar Naidu Parikidona <bhuvaneshparikidona123@gmail.com>
2026-03-12 01:34:26 +0530

    C# Assignment1

---
+++ C# Assignments/Assignment1/Exercise1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    internal class Exercise1
+    {
+        static void DisplayQuotient()
+        {
+            Console.WriteLine("Enter Num1");
+           int num1 = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter Num2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
