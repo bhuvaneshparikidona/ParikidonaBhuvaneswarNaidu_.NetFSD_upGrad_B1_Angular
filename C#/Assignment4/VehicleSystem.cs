89b7fddf9834e5c716bc322db4e9eef17c7f493a
Bhuvaneswar Naidu Parikidona <bhuvaneshparikidona123@gmail.com>
2026-03-17 21:20:27 +0530

    Create VehicleSystem.cs

---
+++ C#/Assignment4/VehicleSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4
+{
+
+    class Vehicle
+    {
+        public string VehicleNumber { get; set; }
+        public string Brand { get; set; }
+
+        public Vehicle(string number, string brand)
+        {
+            VehicleNumber = number;
+            Brand = brand;
+        }
+
+        public void StartVehicle()
+        {
+            Console.WriteLine("Vehicle started");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string FuelType { get; set; }
+
+        public Car(string number, string brand, string fuelType)
+            : base(number, brand)
+        {
+            FuelType = fuelType;
+        }
+    }
+
+    sealed class ElectricCar : Car
+    {
+        public ElectricCar(string number, string brand, string fuelType)
+            : base(number, brand, fuelType)
+        {
+        }
+    }
+
+   
+    // class Tesla : ElectricCar { }  // This will give compilation error
+
+    class VehicleSystem
+    {
+        static void Main()
+        {
+            ElectricCar car = new ElectricCar("AP02BL2028", "Tesla", "Electric");
+            car.StartVehicle();
+            Console.WriteLine(car.VehicleNumber + " " + car.Brand + " " + car.FuelType);
+        }
+    }
+}
